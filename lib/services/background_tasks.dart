42e58b6cb0f97f89b63c31c12258aa2355ebbd54
Xavier Sala <utrescu@gmail.com>
2023-06-10 19:09:40 +0200

    Eliminar llista de pendents

--- lib/services/background_tasks.dart
+++ lib/services/background_tasks.dart
@@ -10,7 +10,7 @@ class BackgroundTask {
 
   Future<void> checkNewNotificacions(dynamic Function(String? p) goto) async {
     await manager.initNotificationManager(goto);
-    var alumnes = await prefs.getAllAlumnes();
+    var alumnes = await prefs.getAlumnesList();
     for (var i = 0; i < alumnes.length; i++) {
       var data = await storage.getAlumne(alumnes[i]);
       if (await api.areNewNotifications(data.token)) {
