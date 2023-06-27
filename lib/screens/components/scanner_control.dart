5c341896c259e355b6f09c75dce035f728059782
Xavier Sala <utrescu@gmail.com>
2023-06-27 09:40:58 +0200

    Permetre tornar a escannejar el QR

--- lib/screens/components/scanner_control.dart
+++ lib/screens/components/scanner_control.dart
@@ -8,21 +8,6 @@ class ScannerControl extends StatelessWidget {
   final double width;
   final VoidCallback scan;
 
-  Widget _botoOutlined(BuildContext context, VoidCallback metode, String text,
-      double height, double width) {
-    return OutlinedButton(
-      style: OutlinedButton.styleFrom(
-        side: BorderSide(color: Theme.of(context).primaryColor),
-        padding: EdgeInsets.symmetric(
-          horizontal: height,
-          vertical: width,
-        ),
-      ),
-      onPressed: metode,
-      child: Text(text),
-    );
-  }
-
   const ScannerControl(
       {Key? key,
       required this.qrkey,
@@ -34,26 +19,53 @@ class ScannerControl extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return qrkey.isValid()
-        ? _showNom(context, qrkey, height, width)
+        ? _showNom(context, qrkey, scan, height, width)
         : _botoOutlined(context, scan, "Escanneja QR", height, width);
   }
 }
 
-Widget _showNom(BuildContext context, Qr qr, double heigth, double width) {
+Widget _botoOutlined(BuildContext context, VoidCallback metode, String text,
+    double height, double width) {
+  return OutlinedButton(
+    style: OutlinedButton.styleFrom(
+      side: BorderSide(color: Theme.of(context).primaryColor),
+      padding: EdgeInsets.symmetric(
+        horizontal: height,
+        vertical: width,
+      ),
+    ),
+    onPressed: metode,
+    child: Text(text),
+  );
+}
+
+Widget _showNom(BuildContext context, Qr qr, VoidCallback metode, double heigth,
+    double width) {
   return Container(
     color: Theme.of(context).primaryColor,
     child: Row(
       children: [
-
         Flexible(
           child: Center(
             heightFactor: 1.1,
-            child: Text(
-              qr.getFullName(),
-              style: TextStyle(
-                fontSize: 20,
-                color: Theme.of(context).colorScheme.onPrimary,
-              ),
+            child: Column(
+              children: [
+                Text(
+                  qr.getFullName(),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.replay_rounded,
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                  onPressed: metode,
+                ),
+              ],
             ),
           ),
         ),
